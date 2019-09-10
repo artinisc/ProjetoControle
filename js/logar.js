32a7177b7c79dd74324222d371da58e3273aeb41
artinisc <artinisc@hotmail.com>
2019-09-10 01:40:01 -0300

    Segunda Importação

--- js/logar.js
+++ js/logar.js
@@ -1,7 +1,7 @@
-function logar(){
+function logar(form){
 
 if( form.nome.value == "betha" && form.senha.value == "sistemas"){
-    location = "./principal.html"
+    location = "./principal.html";
 } else {
     alert("Dados Incorretos!");
 }
