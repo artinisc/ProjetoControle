61146faa1b2bc40a98b7a57cdaafd1fe22be02a9
artinisc <artinisc@hotmail.com>
2019-09-14 01:14:02 -0300

    login: betha senha: sistemas

--- php/busca.php
+++ php/busca.php
@@ -1,59 +1,43 @@
 <?php
-$severname = "localhost";
-$username = "root";
-$password = "";
-$dbname = "projetocontrole";
 
-$conexao = mysql_connect($severname, $username, $password, $dbname);
+include_once("conecta.php");
 
-$requestData = $_REQUEST;
+$pega = "SELECT * FROM ordem ORDER BY ID DESC";
+$resultado = mysqli_query($conecta, $pega);
 
-$columns = array(
-    array( '0' => 'ID' ),
-    array( '1' => 'Nome' ),
-    array( '2' => 'Equipamento' ),
-    array( '3' => 'Entrada' ),
-    array( '4' => 'Situacao' )
-);
+?>
 
-$result_ordems = "SELECT ID, Nome, Equipamento, Entrada, Situacao FROM ordem";
-$resultado_ordems = mysql_query($conexao, $result_ordems);
-$qnt_linhas = mysql_num_rows($resultado_ordems);
+<table class="table">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>EQUIPAMENTO</th>
+            <th>DATA ENT.</th>
+            <th>STATUS</th>
+        </tr>
+    </thead>
+    <tbody>      
+<?php
+if(($resultado) AND ($resultado->num_rows != 0 )){
+    while($linhas = mysqli_fetch_assoc($resultado)){
+        ?>
+            <tr>
+                <th><?php echo $linhas['ID'];  ?></th>
+                <td><?php echo $linhas['Nome'];  ?></td>
+                <td><?php echo $linhas['Equipamento'];  ?></td>
+                <td><?php echo $linhas['Entrada'];  ?></td>
+                <td><?php echo $linhas['Situacao'];  ?></td>
+            </tr>
+        <?php
+    }
 
-$result_ordems = "SELECT ID, Nome, Equipamento, Entrada, Situacao FROM ordem WHERE 1=1";
-if( !empty($requestData['search']['value'])){
-    $resultado_usuarios.=" AND (ID LIKE '".$requestData['search']['value']."%' ";
-    $resultado_usuarios.=" OR Nome LIKE '".$requestData['search']['value']."%' ";
-    $resultado_usuarios.=" OR Equipamento LIKE '".$requestData['search']['value']."%' ";
-    $resultado_usuarios.=" OR Entrada LIKE '".$requestData['search']['value']."%' ";
-    $resultado_usuarios.=" OR Situacao LIKE '".$requestData['search']['value']."%' )";
+}else{
+    print "falha na busca!";
 }
 
-$resultado_ordems = mysql_query($conexao, $result_ordems);
-$totalFiltered = mysql_num_rows($resultado_ordems);
+mysqli_close($conecta);
 
-$result_ordems.="ORDER BY ". $columns[$requestData['order'][0]['column']]."   ".$requestData['order']
-                [0]['dir']." LIMIT ".$requestData['start']." ,".$requestData['length']."   ";
-$resultado_ordems=mysql_query($onexao, $result_ordems);
-
-$dados = array();
-while( $row_ordem = mysql_fetch_array($resultado_usuarios)){
-    $dado = array();
-    $dado[] = $row_ordem["ID"];
-    $dado[] = $row_ordem["Nome"];
-    $dado[] = $row_ordem["Equipamento"];
-    $dado[] = $row_ordem["Entrada"];
-    $dado[] = $row_ordem["Situacao"];
-
-    $dados[] = $dado;
-}
-
-$json_data = array(
-    "draw" => intval($requestData['draw']),
-    "recordsTotal": => intval( $qnt_linhas),
-    "recordsFiltered": => intval($totalFiltered);
-    "data" => $dados
-
-);
-
-echo json_encode($json_data);
+?>
+</tbody>
+</table>
